60b37a75af47e519484237e5bba673c3cad5a385
Maxim Pantelidi <maksim.pantelidi@yandex.ru>
2025-07-07 13:01:28 +0300

    button

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+	<button class="button">
+		<slot />
+	</button>
+</template>
+
+<style scoped>
+.button {
+	border: none;
+	border-radius: 50px;
+	width: 315px;
+	background: var(--color-bg-btn);
+	padding: 16px 10px;
+	font-family: var(--font);
+	font-size: 24px;
+	font-weight: 400;
+	color: var(--primary);
+	cursor: pointer;
+}
+</style>
