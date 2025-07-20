133c77707bcf71b6562c38852ed86c1c9db831f9
Maxim Pantelidi <maksim.pantelidi@yandex.ru>
2025-07-20 14:20:18 +0300

    Добавил карточку и учу2сь испольбзовать события

---
+++ src/components/Score.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Heath from './icons/Heath.vue';
+const num = defineProps({
+	number: String,
+});
+</script>
+
+<template>
+	<div class="score">
+		<div>{{ number }}</div>
+		<Heath />
+	</div>
+</template>
+
+<style scoped>
+.score {
+	display: flex;
+	background: #cce8ff;
+	padding: 12px 24px;
+	max-width: 110px;
+	justify-content: center;
+	border-radius: 36px;
+	gap: 10px;
+	color: #008bfe;
+}
+</style>
